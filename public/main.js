bbf4ef4669ef866018d448a333c1fa2c113e5a49
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-08-17 21:45:46 -0500

    playing with classes

--- public/main.js
+++ public/main.js
@@ -2,4 +2,36 @@
 angular.module("MyApp", [])
 .controller("MainCtrl", function($scope){
   $scope.world = "Testing things out"
+
+
+  class Player {
+    constructor(name) {
+      this.name = name || "Bob";
+      this.items = [];
+      this.monsters = [];
+    }
+
+    rollMove() {
+      return Math.ceil(Math.random()*6);
+    }
+
+  }
+
+  class Monster {
+    constructor() {
+      this.points = 0;
+      this.strength = 0;
+      this.catch = 0;
+    }
+
+    rollAttack(item1, item2) {
+      item1 = item1 ? item1 : 0;
+      item2 = item2 ? item2 : 0;
+      return Math.ceil( (Math.random()*6) + item1 + item2);
+    }
+
+  }
+
+  var testMonster = new Monster()
+  console.log(testMonster)
 })
